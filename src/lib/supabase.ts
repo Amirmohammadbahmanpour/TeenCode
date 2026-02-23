fad5fdd66101ef881ed37faf9ef2a943d37110c7
AmirMohammad Bahmanpour <167961093+Amirmohammadbahmanpour@users.noreply.github.com>
2026-02-23 21:57:28 +0330

    connect project to the host

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,15 @@
 // lib/supabase.ts
 import { createBrowserClient } from '@supabase/ssr'
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+// اضافه کردن یک چک ساده برای جلوگیری از کرش کردن موقع Build
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn("Supabase credentials are missing!")
+}
+
 export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  supabaseUrl || '',
+  supabaseAnonKey || ''
 )
